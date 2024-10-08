d078e46cd7fd0448f5c2e94eccfe595dcc1998d9
Roy Anger <roy@royanger.com>
2024-10-08 13:50:17 -0400

    Updated extension to reflect changes in SDK, added better navigation

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,16 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/chrome-extension"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from "@clerk/chrome-extension"
 import { CountButton } from "~features/count-button"
 
 import "~style.css"
 
 const PUBLISHABLE_KEY = process.env.PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY
+const EXTENSION_URL = chrome.runtime.getURL(".")
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Please add the PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY to the .env.development file')
@@ -11,8 +18,13 @@ if (!PUBLISHABLE_KEY) {
 
 function IndexPopup() {
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-[600px] plasmo-w-[800px] plasmo-flex plasmo-flex-col">
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl={`${EXTENSION_URL}/popup.html`}
+      signInFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+      signUpFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+    >
+      <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-[600px] plasmo-w-[800px] plasmo-flex-col">
         <header className="plasmo-w-full">
           <SignedOut>
             <SignInButton mode="modal" />
